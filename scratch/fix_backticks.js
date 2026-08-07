d4e8128636a78d6d43434bc9a254b541f8fd0d6e
neelieshwar2-del <neelieshwar2-del@users.noreply.github.com>
2026-08-07 15:41:07 +0530

    feat: add admin dashboard interface and remove redundant test and patch files

--- scratch/fix_backticks.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('public/js/main.js', 'utf8');
-c = c.replace(/\\\`/g, '`');
-c = c.replace(/\\\\'/g, "\\'");
-fs.writeFileSync('public/js/main.js', c);
